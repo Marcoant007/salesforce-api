abe2856587274f3f7119304157b25cea7aa93ca0
Marcoant007 <marcoantnovo@gmail.com>
2024-07-02 21:58:38 -0300

    feat: init project and add docker

---
+++ src/main/java/com/marcoant/salesforce_api/SalesforceApiApplication.java
@@ -0,0 +1,13 @@
+package com.marcoant.salesforce_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesforceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesforceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcoant/salesforce_api/SalesforceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcoant.salesforce_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesforceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
